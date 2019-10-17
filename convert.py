2a995acd17b41f7113a3e37a01f333c28a1c1bd6
Vladislav <lobov.vladislaw@yandex.ru>
2019-10-17 22:35:44 +0300

    main.cu added

--- convert.py
+++ convert.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from PIL import Image
 import scipy.misc as smp
 import numpy as np
@@ -13,25 +14,26 @@ if mode == "inverse":
     with open(output_image, "wb") as output:
         w = im.size[0]
         h = im.size[1]
-        output.write(w.to_bytes(4, byteorder='big'))
-        output.write(h.to_bytes(4, byteorder='big'))
+        output.write(w.to_bytes(4, byteorder='little'))
+        output.write(h.to_bytes(4, byteorder='little'))
 
         for i in range(h):
             for j in range(w):
                 rgb = im.getpixel((j, i))
                 for color in rgb:
-                    output.write(color.to_bytes(4, byteorder='big'))
-                output.write((1).to_bytes(4, byteorder='big'))
+                    output.write(color.to_bytes(1, byteorder='little'))
+                output.write((1).to_bytes(1, byteorder='little'))
 else:
     with open(input_image, "rb") as input:
-        w = int.from_bytes(input.read(4), byteorder='big')
-        h = int.from_bytes(input.read(4), byteorder='big')
+        w = int.from_bytes(input.read(4), byteorder='little')
+        h = int.from_bytes(input.read(4), byteorder='little')
         data = np.zeros((h, w, 3), dtype=np.uint8)
 
         for i in range(h):
             for j in range(w):
-                rgba = [int.from_bytes(input.read(4), byteorder='big') for _ in range(4)]
+                rgba = [int.from_bytes(input.read(1), byteorder='little') for _ in range(4)]
                 data[i, j] = rgba[:3]
-        img = smp.toimage(data)
+
+        img = Image.fromarray(data)
         img.save(output_image)
 
--- cpu.py
+++ cpu.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 import scipy.misc as smp
+from PIL import Image
 
 
 def intensity(rgb):
@@ -11,22 +12,22 @@ input_image = sys.argv[1]
 output_image = sys.argv[2]
 
 with open(input_image, "rb") as input, open(output_image, "wb") as output:
-    w = int.from_bytes(input.read(4), byteorder='big')
-    h = int.from_bytes(input.read(4), byteorder='big')
+    w = int.from_bytes(input.read(4), byteorder='little')
+    h = int.from_bytes(input.read(4), byteorder='little')
     data = np.zeros((h, w), dtype=np.int16)
-    dst = np.zeros((h, w, 3), dtype=np.int16)
+    dst = np.zeros((h, w, 3), dtype=np.uint8)
 
     for i in range(h):
         for j in range(w):
-            r = int.from_bytes(input.read(4), byteorder='big')
-            g = int.from_bytes(input.read(4), byteorder='big')
-            b = int.from_bytes(input.read(4), byteorder='big')
-            alpha = int.from_bytes(input.read(4), byteorder='big')
+            r = int.from_bytes(input.read(1), byteorder='little')
+            g = int.from_bytes(input.read(1), byteorder='little')
+            b = int.from_bytes(input.read(1), byteorder='little')
+            alpha = int.from_bytes(input.read(1), byteorder='little')
             data[i, j] = intensity([r, g, b])
     for i in range(h - 1):
         for j in range(w - 1):
             Gx = data[i, j] - data[i + 1, j + 1]
             Gy = data[i, j + 1] - data[i + 1, j]
             dst[i, j] = [min((Gx ** 2 + Gy ** 2) ** 0.5, 255) for _ in range(3)]
-    img = smp.toimage(dst)
-    img.show()
+    img = Image.fromarray(dst)
+    img.save(output_image)
